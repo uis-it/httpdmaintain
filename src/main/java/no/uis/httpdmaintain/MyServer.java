00b06ea4711a8902244ba9aac13d359184671a8d
2904630 <2904630@cb7e345f-c609-e640-855e-630163976756>
2011-02-23 11:30:45 +0000

    - stop function. - java util logging - configurable stop message
    git-svn-id: file:///var/svn/portal/httpdmaintain/trunk@2981 cb7e345f-c609-e640-855e-630163976756

--- src/main/java/no/uis/httpdmaintain/MyServer.java
+++ src/main/java/no/uis/httpdmaintain/MyServer.java
@@ -2,13 +2,17 @@ package no.uis.httpdmaintain;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.InetSocketAddress;
+import java.net.URL;
 import java.nio.CharBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -18,33 +22,76 @@ import com.sun.net.httpserver.HttpServer;
 public class MyServer implements HttpHandler {
 
   private File messageFile;
+  private String stopCmd;
+  private static java.util.logging.Logger log = Logger.getLogger(MyServer.class.getName());
+  private String stopResponse;
 
   public static void main(String[] args) {
     try {
-      int port = 8080;
-      String file = "maintenance.html";
-      if (args.length > 0) {
-        port = Integer.parseInt(args[0]);
-        if (args.length > 1) {
-          file = args[1];
+      int argIdx = 0;
+      String cmd = readArg(args, argIdx++, null);
+      String stopCmd = readArg(args, argIdx++, null);
+      int port = readIntArg(args, argIdx++, 8080);
+      String fileName = readArg(args, argIdx++, "maintenance.html");
+      
+      if (cmd.equals("start")) {
+        new MyServer(stopCmd).startServer(port, fileName);
+        return;
+      } else if (cmd.equals("stop")) {
+        new MyServer(stopCmd).stopServer(port);
+        return;
+      }
+    } catch(Exception e) {
+      log.log(Level.SEVERE, "Starting Server", e);
+    }
+    showUsage();
+  }
+
+  private static String readArg(String[] args, int index, String defaultValue) {
+    if (index < args.length) {
+      return args[index];
+    } else if (defaultValue != null) {
+      return defaultValue;
+    }
+    throw new IllegalArgumentException();
+  }
+
+  private static int readIntArg(String[] args, int index, int defaultValue) {
+    String arg = readArg(args, index, "");
+    if (arg.length() == 0) {
+      return defaultValue;
+    }
+    return Integer.valueOf(arg);
+  }
+  
+  private static void showUsage() {
+    JOptionPane.showMessageDialog(null, "Usage:\n<cmd(start|stop)> <stopCmd> [port(8080)] [html(maintenance.html)]");
+  }
+
+  private MyServer(String stopCmd) {
+    this.stopCmd = "/" + stopCmd;
+    stopResponse = "OK";
+  }
+  
+  private void stopServer(int port) throws IOException {
+    URL url = new URL("http", "localhost", port, this.stopCmd);
+    Object content = url.getContent();
+    if (content instanceof InputStream) {
+      
+      int stopResponseLength = stopResponse.length();
+      byte[] responseBuff = new byte[stopResponseLength+1];
+      int read = ((InputStream)content).read(responseBuff);
+      if (read == stopResponseLength) {
+        
+        String response = new String(responseBuff, 0, read);
+        if (response.equals(this.stopResponse)) {
+          return;
         }
       }
-      new MyServer().startServer(port, file);
-    } catch(Exception e) {
-      writeLog(e);
     }
+    throw new IllegalStateException();
   }
-
-  private static void writeLog(Exception e) {
-    try {
-      FileWriter fw = new FileWriter("maintenance.log", true);
-      e.printStackTrace(new PrintWriter(fw));
-      fw.close();
-    } catch(Exception ex) {
-      // ignore
-    }
-  }
-
+  
   private void startServer(int port, String msgFile) throws Exception {
     File file = new File(msgFile);
     if (file.canRead()) {
@@ -52,18 +99,23 @@ public class MyServer implements HttpHandler {
     } else {
       throw new IllegalArgumentException("File " + msgFile + " does not exist");
     }
+    log.info("Starting server");
     HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
     server.createContext("/", this);
     server.setExecutor(null);
     server.start();
+    log.info("Server started");
   }
 
   @Override
   public void handle(HttpExchange httpEx) throws IOException {
     if (receivedStop(httpEx)) {
+      log.info("Received Stop Request");
+      sendMessage(httpEx, "text/plain", stopResponse);
       httpEx.getHttpContext().getServer().stop(0);
     } else {
+      log.info("Received request from " + httpEx.getRemoteAddress().toString());
       FileReader fr = new FileReader(this.messageFile);
       StringWriter sw = new StringWriter();
       CharBuffer cb = CharBuffer.allocate(300);
@@ -73,16 +125,21 @@ public class MyServer implements HttpHandler {
         sw.append(s, 0, nboRead);
       }
       String result = sw.toString();
-      httpEx.sendResponseHeaders(200, result.length());
-
-      OutputStream os = httpEx.getResponseBody();
-      os.write(result.getBytes());
-      os.close();
+      sendMessage(httpEx, "text/html", result);
     }
   }
 
+  private void sendMessage(HttpExchange httpEx, String contentType, String response) throws IOException {
+    httpEx.getResponseHeaders().add("Content-Type", contentType);
+    httpEx.sendResponseHeaders(200, response.length());
+
+    OutputStream os = httpEx.getResponseBody();
+    os.write(response.getBytes());
+    os.close();
+  }
+
   private boolean receivedStop(HttpExchange httpEx) throws IOException {
-    if (httpEx.getRequestMethod().equals("GET") && httpEx.getRequestURI().toString().equals("/STOP")) {
+    if (httpEx.getRequestMethod().equals("GET") && httpEx.getRequestURI().toString().equals(this.stopCmd)) {
       return true;
     }
     return false;
